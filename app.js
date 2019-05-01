ed19a2e8805cca0e478dbd5b8a6ada81a44551ed
aliciay <alicia.yoosin@gmail.com>
2019-05-01 08:48:09 -0400

    switch to use anime.js as animation lib instead of paper.js, this is a first trying out of anime.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var express = require("express");
 var app = express();
 var bodyPaser = require("body-parser");
+var path = require("path");
 
 app.use(bodyPaser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+//app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.get("/", (req, res)=>{
 
-  res.render("newindex");
+  //res.render("newindex");
+  res.sendfile('myindex.html')
 });
 
 app.listen(3000,  ()=> {
