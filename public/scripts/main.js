f9f97a6353614266b0b7d99dfb37353b872a7eaa
aliciay <alicia.yoosin@gmail.com>
2019-05-19 20:19:42 -0400

    added popup modal to show, copy and share saved link

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,6 +4,7 @@ import * as ctrl from "./controller.js"
 var s = document.getElementById('svgSpace');
 var sidebar = document.getElementById('sidebar');
 var playground = document.getElementById('playground');
+
 /////////////// functions ////////////////////////
 
 function toggleSidebar(){
@@ -28,6 +29,25 @@ function onDocumentDrop_noDrop(event) {
   return false;
 }
 
+var sharinglink = document.location.href;
+
+function shareSNS(link, name){
+  var d=document,
+  e=encodeURIComponent,
+  s1=window.getSelection,
+  s2=d.getSelection,
+  s3=d.selection,
+  s=s1?s1():s2?s2():s3?s3.createRange().text:'',
+  r=link+e(sharinglink)+'&title='+e(d.title),
+  x=function(){
+    if(!window.open(r,name,'toolbar=0,resizable=1,scrollbars=yes,status=1,width=450,height=330'))location.href=r+'&r=1'};
+    if(/Firefox/.test(navigator.userAgent)){
+      setTimeout(x,0)
+    }else{
+      x()
+    }
+  }
+
 function postCtrls(){
   var xhr = new XMLHttpRequest();
   xhr.open("POST", 'http://localhost:3000/saveShare', true);
@@ -36,7 +56,8 @@ function postCtrls(){
 
   xhr.onreadystatechange = function () {
     if(xhr.readyState === 4 && xhr.status === 200) {
-      console.log('http://localhost:3000/?id=' +xhr.responseText);
+      document.getElementById("savedLink").value = 'http://localhost:3000/?id=' +xhr.responseText;
+      sharinglink = 'http://localhost:3000/?id=' +xhr.responseText;
     }
   };
   var s= JSON.stringify(ctrl.ctrls);
@@ -44,6 +65,12 @@ function postCtrls(){
   xhr.send(s);
   //{"obj": JSON.stringify(ctrl.ctrls)}
 
+}
+
+function copyLink() {
+  var copyText = document.getElementById("savedLink");
+  copyText.select();
+  document.execCommand("copy");
 }
 
 ///////////// Init ///////////////////////////
@@ -57,6 +84,10 @@ document.addEventListener('drop', onDocumentDrop_noDrop, false);
 ctrl.initCtrl();
 document.getElementById('addNewCtrl').addEventListener('click', ctrl.addNewCtrl, false);
 document.getElementById('saveShare').addEventListener('click', postCtrls, false);
+document.getElementById('shareWeibo').addEventListener('click', () => {shareSNS('http://service.weibo.com/share/share.php?url=','Weibo')}, false);
+document.getElementById('shareFB').addEventListener('click', () => {shareSNS('https://www.facebook.com/sharer/sharer.php?u=', 'Facebook')}, false);
+document.getElementById('shareTwitter').addEventListener('click', () => {shareSNS('https://twitter.com/share?url=', 'Twitter')}, false);
+document.getElementById('copyLink').addEventListener('click', copyLink, false);
 
 // sidebar.addEventListener('dragleave', onDocumentDrag, false);
 
